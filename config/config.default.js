aa1aa2c81a71326c40ca8df68c78e50ba03faa05
wb-mz726035 <wb-mz726035@alibaba-inc.com>
2022-08-09 10:12:15 +0800

    add

--- config/config.default.js
+++ config/config.default.js
@@ -63,3 +63,11 @@ exports.alinode = {
   appid: '91364',
   secret: '8e211f875ab87fefc00e885c5ccce60c96389966',
 };
+
+exports.cluster = {
+  listen: {
+    port: 7002,
+    hostname: '127.0.0.1', // 不建议设置 hostname 为 '0.0.0.0'，它将允许来自外部网络和来源的连接，请在知晓风险的情况下使用
+    // path: '/var/run/egg.sock',
+  },
+};
